d8a3ec8892eab02cb6a4b615428bc8ab5b764979
Dmitry Smolin <dimsmol@gmail.com>
2013-03-19 19:13:05 +0400

    added missed ';' in client/export/

--- client/apis.js
+++ client/apis.js
@@ -1,5 +1,5 @@
 ;(function(e,t,n,r){function i(r){if(!n[r]){if(!t[r]){if(e)return e(r);throw new Error("Cannot find module '"+r+"'")}var s=n[r]={exports:{}};t[r][0](function(e){var n=t[r][1][e];return i(n?n:e)},s,s.exports)}return n[r].exports}for(var s=0;s<r.length;s++)i(r[s]);return i})(typeof require!=="undefined"&&require,{1:[function(require,module,exports){
-"use strict"
+"use strict";
 var apis = require('../../lib/client');
 
 
